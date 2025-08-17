fa66dbb67a909c45425c2391b10b1a3c4a552f89
Rohama-Majeed <rohamamajeed3@gmail.com>
2025-08-17 17:42:56 +0500

    change

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "/api",
+    baseURL: "https://blog-app-mrk6.vercel.app/api",
 })
 // https://blog-app-pcw5.vercel.app

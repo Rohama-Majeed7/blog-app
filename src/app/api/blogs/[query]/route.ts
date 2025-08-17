6ddf08234d52049a02ab37ce609140497e53167d
Rohama-Majeed <rohamamajeed3@gmail.com>
2025-08-17 11:59:56 +0500

    change

--- src/app/api/blogs/[query]/route.ts
+++ src/app/api/blogs/[query]/route.ts
@@ -1,10 +1,10 @@
 import { prisma } from "@/lib/prisma";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { query: string } }
+     req: Request,
+  context: { params: Promise<{ query: string }> }
 ) {
-  const { query } =  params;
+  const { query } =  await context.params;
   const blog = await prisma.post.findMany({
     where: {
       OR: [{ category: { contains: query } }, { title: { contains: query } }],

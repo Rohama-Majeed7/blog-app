c075507d313b3e2cb566992df1083d14e88f9c63
Rohama Majeed <rohamamajeed3@gmail.com>
2026-06-26 17:07:04 +0300

    files set

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,12 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const connectionString = process.env.DATABASE_URL!;
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient()
+const adapter = new PrismaPg({
+  connectionString,
+});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = new PrismaClient({
+  adapter,
+});
